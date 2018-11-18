b6e2f19e9df18cc5decc1f9812c210d714cdba40
Cony <cony@hiddentrack.co>
2018-11-18 21:27:35 +0900

    추가: 날씨 케이스, 아이콘

--- Weather.js
+++ Weather.js
@@ -1,24 +1,81 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import PropTypes from 'prop-types';
 
-export default class Weather extends Component {
-  render () {
-    return (
-      <LinearGradient colors={['#00C6FB', '#005BEA']} style={styles.container}>
-        <View style={styles.upper}>
-          <Ionicons color='white' size={144} name='ios-rainy'/>
-          <Text style={styles.temp}>35</Text>
-        </View>
-        <View style={styles.lower}>
-          <Text style={styles.title}>Raning like a MF</Text>
-          <Text style={styles.subtitle}>For more info look outside</Text>
-        </View>
-      </LinearGradient>
-    )
-  }
+const WeatherCases = {
+  Rain: {
+    colors: ['#00C6FB', '#005BEA'],
+    title: 'Raining like a MF',
+    subtitle: 'For more info look outside',
+    icon: 'weather-rainy',
+  },
+  Clear: {
+    colors: ['#FEF253', '#FF7300'],
+    title: 'Sunny as fuck',
+    subtitle: 'Go get your ass burnt',
+    icon: 'weather-sunny',
+  },
+  Thunderstorm: {
+    colors: ['#00ECBC', '#007ADF'],
+    title: 'Thunderstorm in the house',
+    subtitle: 'Actually, outside of the house',
+    icon: 'weather-lightning',
+  },
+  Clouds: {
+    colors: ['#D7D2CC', '#304352'],
+    title: 'Clouds',
+    subtitle: 'I know, fucking boring',
+    icon: 'weather-cloudy',
+  },
+  Snow: {
+    colors: ['#7DE2FC', '#B9B6E5'],
+    title: 'Cold as balls',
+    subtitle: 'Do you want build a snowman? Fuck no.',
+    icon: 'weather-snowy',
+  },
+  Drizzle: {
+    colors: ['#89F7FE', '#66A6FF'],
+    title: 'Drizzle',
+    subtitle: 'Is like rain, but gay 🏳‍🌈',
+    icon: 'weather-hail',
+  },
+  Haze: {
+    colors: ['#89F7FE', '#66A6FF'],
+    title: 'Haze',
+    subtitle: 'Dont know what that is 💩',
+    icon: 'weather-hail',
+  },
+  Mist: {
+    colors: ['#D7D2CC', '#304352'],
+    title: 'Mist!',
+    subtitle: 'Its like you have no glasses on.',
+    icon: 'weather-fog',
+  },
 }
+
+function Weather ({ weatherName, temp }) {
+  console.log(weatherName);
+  return (
+    <LinearGradient colors={ WeatherCases[weatherName].colors } style={styles.container}>
+      <View style={styles.upper}>
+        <MaterialCommunityIcons color='white' size={144} name={ WeatherCases[weatherName].icon }/>
+        <Text style={styles.temp}>{ temp }℃</Text>
+      </View>
+      <View style={styles.lower}>
+        <Text style={styles.title}>{ WeatherCases[weatherName].title }</Text>
+        <Text style={styles.subtitle}>{ WeatherCases[weatherName].subtitle }</Text>
+      </View>
+    </LinearGradient>
+  )
+}
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired
+};
+
+export default Weather;
 
 const styles = StyleSheet.create({
   container: {
