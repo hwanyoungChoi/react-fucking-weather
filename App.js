ac4b903ddf8fac9ad08f6acbe026a8f355b3e1e8
Cony <cony@hiddentrack.co>
2018-11-18 20:58:10 +0900

    추가: 날씨 api

--- App.js
+++ App.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Weather from './Weather';
 
+const API_KEY = 'a0369cf32e0704eac0b4b391352a6871';
+
 export default class App extends Component {
   state = {
     isLoaded: false,
     error: null,
+    temperature: null,
+    name: null,
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true,
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
         this.setState({
           error: error,
         })
+      });
+  }
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true,
+        });
       });
   }
   render() {
